dac03b8472b5ededd84cf97075df5368466ea409
hisava <veretennikovalexey@gmail.com>
2024-06-07 09:50:40 +0300

    alex

---
+++ 3/3.1/p4.py
@@ -0,0 +1,4 @@
+hello_python = input()
+i_love_you = input()
+res = hello_python + " " + i_love_you
+print( res )
---
+++ 3/3.1/p7.py
@@ -0,0 +1,5 @@
+hello, python = map( str, input().split() )
+hello = hello + " "
+python = python + " "
+res = hello * 2 + python * 3
+print( res )
