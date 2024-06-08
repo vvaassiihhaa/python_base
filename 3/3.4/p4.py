29dc78f8b5675ecc70123176202766c955c784d8
hisava <veretennikovalexey@gmail.com>
2024-06-08 21:20:29 +0300

    Commit Message

---
+++ 3/3.4/p4.py
@@ -0,0 +1,7 @@
+s = input()
+s = s.replace( " ", "\'", 1 )
+res = s.replace( " ", "\"" )
+# f = s2.find( '\"' )
+# s2[f] = "\'"
+# res = s2[:f] + "'" + s2[f+1:]
+print( res )
---
+++ 3/3.4/p5.py
@@ -0,0 +1,3 @@
+path = r"C:\WINDOWS\System32\drivers\etc\hosts"
+print ( path )
+# C:\WINDOWS\System32\drivers\etc\hosts
