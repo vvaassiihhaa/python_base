35df76e7fe21627e0a3565551f75521d7fc2dd1b
hisava <veretennikovalexey@gmail.com>
2024-06-22 11:18:07 +0300

    Commit Message

--- 5/5.3/9.py
+++ 5/5.3/9.py
@@ -1,16 +1,21 @@
 cities = input().split()
-last = ""
-res = "НЕТ"
+last = cities[0][0].lower()
+res = "ДА"
 for city in cities:
-    if last == city[0].lower():
+    if last == city[0].lower() and res == "ДА":
         res = "ДА"
-        break
+    else:
+        res = "НЕТ"
     last = city[-1]
     if last in 'ьъы':
         last = city[-2]
 print( res )
 #    print( city[-1] )
 #    print( city )
+
+# test #2
+# input: Вологда Архангельск Курск Москва
+# output: НЕТ
 
 # ьъы
 # Москва Астрахань Новгород Димитровград Душанбе
